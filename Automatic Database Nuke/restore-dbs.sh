d2bdb7b94032dd83a6688a36c78669399a14917f
Miguel Callejas <miguelcallejasp@gmail.com>
2017-11-01 17:38:48 -0400

    Restoring Databases are different from Kafka CleanUp

--- Automatic Database Nuke/restore-dbs.sh
+++ Automatic Database Nuke/restore-dbs.sh
@@ -8,12 +8,7 @@ echo "Deleting MySQL database and creating a clean one"
 ./deletemysql.sh
 
 echo "Deleting Zookeeper and Kafka data"
-docker-compose stop zookeeper kafka
-rm -rf data/zookeeper data/kafka
 ls -l data/
-docker-compose up -d zookeeper
-sleep 5
-docker-compose up -d kafka
 docker-compose stop hazelcast
 docker-compose up -d hazelcast
-docker-compose logs -f --tail 200 mongo mysql kafka zookeeper hazelcast
+docker-compose logs -f --tail 200 mongo mysql hazelcast
